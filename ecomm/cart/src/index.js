debc9525e2c94e3667a5ddb867b002f3785b4589
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-06-29 11:56:31 +0000

    faker added in the shared array of ModuleFederatedPlugin and bootstrap.js created for async load of index.js

--- ecomm/cart/src/index.js
+++ ecomm/cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from "faker";
-
-let carts = `<div>You have ${faker.random.number()} items in your cart </div>`
-
-document.querySelector('#dev-carts').innerHTML = carts;
+import("./bootstrap");
--- ecomm/container/webpack.config.js
+++ ecomm/container/webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
         remotes: {
             products: "products@https://cuddly-space-funicular-x55g7jpvjppvfv47w-8081.app.github.dev/remoteEntry.js",
             carts: "carts@https://cuddly-space-funicular-x55g7jpvjppvfv47w-8082.app.github.dev/remoteEntry.js",
+        },
+        shared: {
+            
         }
        }),
        new HtmlWebpackPlugin({
